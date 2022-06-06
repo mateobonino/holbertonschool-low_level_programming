12afc9e95edf64e4c0c38d1012571bad360f6c47
Mateo <5077@holbertonschool.com>
2022-06-06 06:15:16 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+* main - prints "Programming is like building a multilingual puzzle"
+* @void: no args passed
+*
+*
+*Return: the function will return error code "0"
+*
+*
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
