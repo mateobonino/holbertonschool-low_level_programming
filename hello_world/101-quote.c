6a086baadc00e279d1f90b29ae94762555eef801
Mateo <5077@holbertonschool.com>
2022-06-06 08:41:28 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
 * @void: no args passed
@@ -10,7 +10,6 @@
 */
 int main(void)
 {
-	char myString = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
