9d00a72ba086482bebcdd0b7eae363377074dcef
Mateo <5077@holbertonschool.com>
2022-06-06 13:05:35 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints numbers from 0 to 9
+* @void: no args passed
+*
+*
+*Return: the function will return error code "0"
+*
+*
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
+
