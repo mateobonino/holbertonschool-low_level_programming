c90d2e18c47064982cc2811725e8e1ceb86113ba
Mateo <5077@holbertonschool.com>
2022-06-06 06:19:27 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints "with proper grammar, but the outcome is a piece of art,"
+* @void: no args passed
+*
+*
+*Return: the function will return error code "0"
+*
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
