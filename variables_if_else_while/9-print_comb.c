fb9e23237854143fda84093ccb568b3b4d30313b
Mateo <5077@holbertonschool.com>
2022-06-06 14:25:03 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digits numbers.
+* @void: no args passed
+*
+*
+*Return: the function will return error code "0"
+*
+*
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
