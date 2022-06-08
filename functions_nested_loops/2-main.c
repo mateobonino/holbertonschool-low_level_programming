34ff49434d1228cbdca0a746c402e89f7bf11255
Mateo <5077@holbertonschool.com>
2022-06-08 16:38:21 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - calls print_alphabet_x10()
+* @void: no args
+*
+*
+*Return: no return, void type function
+*
+*
+*/
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints entire alphabet from A to Z in lowercase 10 times
+* @void: no args
+*
+*
+*Return: no return, void type function
+*
+*
+*/
+void print_alphabet_x10(void)
+{
+	int times;
+
+	for (times = 0; times < 10; times++)
+	{
+		print_alphabet();
+	}
+}
