967b2b47d08d6f9d1e23f1a24d0f17728349df39
Mateo <5077@holbertonschool.com>
2022-06-08 14:13:40 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,6 +8,17 @@
 *
 *
 */
+void print_alphabet(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return;
+}
+
 int main(void)
 {
 	print_alphabet();
