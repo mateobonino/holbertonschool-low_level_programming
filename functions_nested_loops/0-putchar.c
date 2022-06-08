1bd47d50dd3492b5baf980e0ecb3383f436bc100
Mateo <5077@holbertonschool.com>
2022-06-08 09:34:21 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,23 @@
 #include "main.h"
 /**
- * main - This C program prints _putchar, followed by a new line
- * return: zero (0)
- */
+* main - prints "_putchar"
+* @void: no args passed
+*
+*
+*Return: the function will return error code "0"
+*
+*
+*/
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
