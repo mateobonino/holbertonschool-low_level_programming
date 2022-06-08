e800f11506f1af1bdecc205a3bc7e2ba48284985
Mateo <5077@holbertonschool.com>
2022-06-08 14:06:47 -0700

    1-alphabet.c

---
+++ functions_nested_loops/print_alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet in lowercase
+* @void: no args passed
+*
+*
+*Return: no return, void function
+*
+*
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return;
+}
