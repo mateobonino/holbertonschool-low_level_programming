97955d034bfc3782505f24730c377d26e4b3ce4e
Mateo <5077@holbertonschool.com>
2022-06-09 14:45:37 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @a:first character to multiply
+ * @b: second character to multiply
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+	int total = a * b;
+
+	return (total);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _isupper(int);
 int _putchar(char c);
 int _isdigit(int);
+int mul(int, int);
