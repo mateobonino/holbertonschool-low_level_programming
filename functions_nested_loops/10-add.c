ea45b4bd0eb180914db39098e58921ecd018a8bd
Mateo <5077@holbertonschool.com>
2022-06-09 06:39:55 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* add - adds two ints
+* @a: first number to add
+* @b: 2nd number to add
+*
+*Return: no return, void type function
+*
+*
+*/
+int add(int a, int b)
+{
+	int ret;
+
+	ret = a + b;
+
+	return (ret);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* times_table - prints times table 9 times
+* @void: no args
+*
+*
+*Return: no return, void type function
+*
+*
+*/
+void times_table(void)
+{
+	int times;
+
+	for (times = 0; times <= 9; times++)
+	{
+		_putcha
+		_putcharr
+	}
+}
