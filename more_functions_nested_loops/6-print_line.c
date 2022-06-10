1c7119b600069533d84a56e0b1fbca6559f87d80
Mateo <5077@holbertonschool.com>
2022-06-09 18:12:55 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_line - prints the character '_' followed by a new line
+* @n: number of times to print '_'
+*
+*Return: no return, void type function
+*
+*
+*/
+void print_line(int n)
+{
+	int times;
+
+	for (times = 0; times <= n; times++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
