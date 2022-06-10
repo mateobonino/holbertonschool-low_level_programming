9d49509de1b760bbc6afb3afb4d051c998ec46be
Mateo <5077@holbertonschool.com>
2022-06-10 05:10:27 -0700

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,16 +11,16 @@ void print_square(int size)
 {
 	int x, y;
 
-	if (size > 0)
+	for (x = 1; x <= size; x++)
 	{
-		for (x = 1; x <= size; x++)
+		for (y = 1; y <= size; y++)
 		{
-			for (y = 1; y <= size; y++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
