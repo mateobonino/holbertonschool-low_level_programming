5ab56ef8cf92643bf01d12b0c49f50b2056c7551
Mateo <5077@holbertonschool.com>
2022-06-17 15:13:08 -0700

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i++])
+	{
+		j++;
+	}
+	for (i = 0; src[i]; i++)
+	{
+		dest[j++] = src[i];
+	}
+	return (dest);
+}
