6da6d08de95f233f7ef6395364617bbf4646ecc7
Mateo <5077@holbertonschool.com>
2022-06-19 19:47:35 -0700

    5-strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* *_strstr - locates a substring
+* @haystack: the string to search
+* @needle: the substring to search
+*Return: null if the string is not found
+*
+*
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+			if (needle[j + 1] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
