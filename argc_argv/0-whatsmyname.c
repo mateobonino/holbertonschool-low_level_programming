1df41baffbf3dbee69f0dbca4f99621d141020f1
Mateo <5077@holbertonschool.com>
2022-06-21 04:57:10 -0700

    0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints the name of he program followed by a new line
+* @argc: the argv array size
+* @argv: contains an array with the arguments passed by argc
+*Return: 0
+*
+*/
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
