06b276f97c20ca4d4ee98f9a93d1c706155664b2
Mateo <5077@holbertonschool.com>
2022-06-22 15:25:00 -0700

    Task 1-strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,14 +12,13 @@ char *_strdup(char *str)
 	int j, size;
 	char *p;
 
-	for (size = 0; str[size] != '\0'; size++)
-	{
-		;
-	}
-
 	if (str == NULL)
 	{
 		return (NULL);
+	}
+	for (size = 0; str[size] != '\0'; size++)
+	{
+		;
 	}
 	p = malloc(sizeof(char) * size + 1);
 	if (p == NULL)
