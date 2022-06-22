57b85a533c46ecc397391932d0e784adc6b76f81
Mateo <5077@holbertonschool.com>
2022-06-22 09:00:47 -0700

    Task exec_files/

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *create_array - creates an array an initilizates with specific char
+* @size: the size of the array
+* @c: the char to initializate
+*Return: if size = 0, returns NULL, otherwise, returns the character
+*
+*
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int n;
+
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(arr) * size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (n = 0; n < size; n++)
+		{
+			arr[n] = c;
+		}
+	}
+	return (arr);
+}
