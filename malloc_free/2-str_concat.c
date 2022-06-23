180d716fdbb01774e84bbf8826c92a48714afa48
Mateo <5077@holbertonschool.com>
2022-06-22 21:08:02 -0700

    Task 2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,13 +14,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = "";
+		s1 = "";
 	} else if (s2 == NULL)
 	{
-		*s2 = "";
-	} else if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
+		s2 = "";
 	}
 	s1len = charlen(s1);
 	s2len = charlen(s2);
