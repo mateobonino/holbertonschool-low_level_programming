748d13d89f123a57d7294f69d7afaacba193b2ff
Mateo <5077@holbertonschool.com>
2022-06-23 14:36:30 -0700

    Task 3-alloc_grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,32 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
-/*
-*
-*
+/**
+* **alloc_grid - returns a pointer to a 2d array
+* @width: the array width
+* @height: the array height
+*Return: NULL if w, h or a = NULL, a otherwise
 *
 *
 */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	char *a;
+	int n, m;
+	int **a;
 
-	if (widht <= 0 || height <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	a = malloc(sizeof(int) * width);
+	a = malloc(sizeof(int) * height);
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < heigth; i++)
+	for (n = 0; n < height; n++)
 	{
-		a[i] = malloc(sizeof(int) * heigth);
-		if (a[i] == NULL)
+		a[n] = malloc(sizeof(int) * width);
+		if (a[n] == NULL)
 		{
+			free(a);
+			for (m = 0; m <= n; m++)
+			{
+				free(a[m]);
+			}
 			return (NULL);
-			for (j = 0;)
+		}
+		for (m = 0; m < width; m++)
+		{
+			a[n][m] = 0;
 		}
 	}
+	return (a);
 }
