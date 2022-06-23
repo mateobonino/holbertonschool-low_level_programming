fd1fbf80bc0c449247c0c574c63044e842da29ce
Mateo <5077@holbertonschool.com>
2022-06-22 20:59:47 -0700

    Task 2-str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* *str_concat - concatenates two strings
+* @s1: first string
+* @s2: second string
+*Return: returns the concatenated string
+*
+*/
+char *str_concat(char *s1, char *s2)
+{
+	long int i, j, s1len, s2len;
+	char *a;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	s1len = charlen(s1);
+	s2len = charlen(s2);
+	a = malloc(sizeof(char) * s1len + s2len + 1);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < s1len; i++)
+	{
+		a[i] = s1[i];
+	}
+	for (j = 0; j < s2len; j++)
+	{
+		a[i] = s2[j];
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+}
+
+/**
+* charlen - gets the length of a string
+* @s: the string
+*Return: returns the length of the string
+*
+*
+*/
+int charlen(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+	{
+		;
+	}
+	return (size);
+}
