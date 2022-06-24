375249d6d64081a582b7a5661bca07fdf49e2deb
Mateo <5077@holbertonschool.com>
2022-06-24 14:45:07 -0700

    Task 4-free_grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* free_grid - frees a 2 dimensional grid
+* @grid: the grid
+* @height: the array height
+*Return: 0;
+*
+*
+*/
+void free_grid(int **grid, int height)
+{
+	int n;
+
+	for (n = 0; n < height; n++)
+	{
+		free(grid[n]);
+	}
+	free(grid);
+	return (0);
+}
