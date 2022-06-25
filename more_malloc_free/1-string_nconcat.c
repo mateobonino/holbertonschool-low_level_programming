50f36816fffbf99166dcd7b83fca227f7e49ef2e
Mateo <5077@holbertonschool.com>
2022-06-25 16:49:38 -0700

    Task 1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (a = 0; a < size1; a++)
 		p[a] = s1[a];
 
-	for (b = 0; s2[b] != '\0'; b++)
+	for (b = 0; b < size2; b++)
 	{
 		p[a] = s2[b];
 		a++;
