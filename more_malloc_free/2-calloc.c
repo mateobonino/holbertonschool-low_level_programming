9f4f91c37d8b42a63a40736a4bde34a13e9c8f8c
Mateo <5077@holbertonschool.com>
2022-06-25 20:36:56 -0700

    Task 2-calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,17 +10,21 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char arr[nmemb];
-	int *p;
+	char *p;
+	int a;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(arr) * size);
+	p = malloc(sizeof(char) * (size * nmemb));
 	if (p == NULL)
 	{
 		return (NULL);
+	}
+	for (a = 0; a < nmemb * size; a++)
+	{
+		p[a] = '\0';
 	}
 	return (p);
 }
