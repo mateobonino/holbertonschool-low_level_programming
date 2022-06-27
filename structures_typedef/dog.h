03b8d776ae206fcc95b60aa1930249a12a7df108
Mateo <5077@holbertonschool.com>
2022-06-27 14:14:15 -0700

    Task dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - struct containing info about a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: dog owner
+*Return: returns the concatenated string
+*
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
