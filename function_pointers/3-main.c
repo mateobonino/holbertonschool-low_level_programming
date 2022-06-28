ca096710ea21be412baebe96ae0a7a1569ff2b2f
Mateo <5077@holbertonschool.com>
2022-06-28 14:56:48 -0700

    Task calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -17,19 +17,19 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(97);
+		exit(98);
 	}
 	/*printf("argc = 4\n");*/
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	/*printf("argv[2] != NULL\n");*/
 	if (((*argv[2]) == '/' || (*argv[2]) == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 	/*printf("argv[1] and argv[3] are digits\n");*/
 	res = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
