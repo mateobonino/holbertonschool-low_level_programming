0cae6ee8b6ab2fcdd860749c0e8aeb29935a3af7
Mateo <5077@holbertonschool.com>
2022-06-30 22:15:16 -0700

    Task 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -42,7 +42,7 @@ void print_all(const char * const format, ...)
 		}
 		if ((format[i] == 'f' || format[i] == 'i' ||
 			format[i] == 's' || format[i] == 'c') && format[i + 1] != '\0')
-			printf(",");
+			printf(", ");
 		i++;
 	}
 	printf("\n");
