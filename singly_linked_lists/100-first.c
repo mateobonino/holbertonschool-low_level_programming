1d2d0b7a44b80b20386a51436192e201afc858d1
Mateo <mbonino1810@gmail.com>
2022-07-13 10:39:26 -0400

    Task 6

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+* first - prints something before main() func
+*Return: void function
+*/
+void __attribute__((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
