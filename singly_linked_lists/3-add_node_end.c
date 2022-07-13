428e2921db2e5801ce528376723e9d23e866e985
mateo <mateo@localhost.localdomain>
2022-07-13 09:58:01 -0400

    Task 4

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+* *add_node_end - adds node to the end of the list
+* @head: the list first item
+* @str: the item to add
+*Return: the list
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i;
+	char *node;
+	list_t *newNode, *isLast = *head;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	node = strdup(str);
+	if (node == NULL)
+	{
+		free(newNode);
+		free(node);
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	newNode->str = node;
+	newNode->next = NULL;
+	newNode->len = i;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+	}
+	else
+	{
+		while (isLast != NULL && isLast->next != NULL)
+		{
+			isLast = isLast->next;
+		}
+		isLast->next = newNode;
+	}
+	return (*head);
+}
