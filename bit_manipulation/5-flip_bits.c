9bf790c6da284a1e282fa37f120665f0d91dbb70
Mateo <mbonino1810@gmail.com>
2022-07-19 09:17:22 -0400

    Task 6

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* flip_bits - counts the number of bits needed to get other number
+* @n: the first number
+* @m: the second number
+* Return: the number of bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int total = n ^ m, i = 0;
+
+	while (total)
+	{
+		total &= total - 1;
+		i++;
+	}
+	return (i);
+}
