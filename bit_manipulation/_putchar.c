e7a87d37e539a4c40cb48d79e8b4e378477c24a8
Mateo <mbonino1810@gmail.com>
2022-07-19 08:19:17 -0400

    Task 1

---
+++ bit_manipulation/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _putchar - writes a char :)
+* @a: the char to print
+* Return: void
+*/
+void _putchar(char a)
+{
+	write(1, &a, 1);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int n);
+void _putchar(char a);
 
 #endif /*MAIN_H*/
