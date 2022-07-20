8c19b9cb2b7b1b51fdf38b409849e57190bdf914
Mateo <mbonino1810@gmail.com>
2022-07-20 01:05:47 -0400

    Task 2

---
+++ file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* create_file - creates a file
+* @filename: the name of the file to create
+* @text_content: NULL terminated string to write to the file
+* Return: 1 on success, -1 if failed
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int wr = 0, file_desc = 0, ret = 0;
+
+	if (!filename)
+		return (-1);
+	file_desc = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (file_desc == -1)
+		return (0);
+	for (; text_content != NULL && text_content[ret] != '\0'; ret++)
+		;
+	wr = write(file_desc, text_content, ret);
+	if (wr == -1)
+		return (-1);
+	close(file_desc);
+	return (1);
+
+
+}
