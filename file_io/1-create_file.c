e9ef6fe6e15164864893cecec4621829a63580a4
Mateo <mbonino1810@gmail.com>
2022-07-20 01:34:24 -0400

    Task 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file_desc = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	file_desc = open(filename, O_WRONLY | O_CREAT, 600);
 	if (file_desc == -1)
 		return (-1);
 	for (; text_content != NULL && text_content[ret] != '\0'; ret++)
