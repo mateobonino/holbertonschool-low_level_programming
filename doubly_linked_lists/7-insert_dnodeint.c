433660cc7794b879c92864e2758f815bfaa99e3d
Mateo <mbonino1810@gmail.com>
2022-07-22 12:12:48 -0400

    Task 8

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - inserts a node at a given position
+* @h: the list items
+* @idx: the index
+* @n: the node data
+* Return: the address of the node, or NULL if it failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode, *temp = *head;
+	unsigned int cnt = 0;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->n = n;
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h , n));
+	}
+	while (cnt < idx - 1)
+	{
+		if (temp == NULL || temp->next == NULL)
+		{
+			free(newNode);
+			return (NULL);
+		}
+		temp = temp->next;
+		cnt++;
+	}
+	newNode->next = temp->next;
+	newNode->prev = temp;
+	if (temp ->next == NULL)
+		temp->next = newNode;
+	else
+		temp->next->prev = newNode;
+	return (newNode);
+}
