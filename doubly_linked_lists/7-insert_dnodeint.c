798d32505167a740a092eaf661d09caa95c3b940
Mateo <mbonino1810@gmail.com>
2022-07-22 12:13:31 -0400

    Task 8

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode, *temp = *head;
+	dlistint_t *newNode, *temp = *h;
 	unsigned int cnt = 0;
 
 	newNode = malloc(sizeof(dlistint_t));
