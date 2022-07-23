c811b2cc105a16a94f52c24a5cc9542efd13be1c
Mateo <mbonino1810@gmail.com>
2022-07-22 22:26:24 -0400

    Task 9

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -17,7 +17,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = temp->next;
 		temp->prev = NULL;
 		free(temp);
-		return (1)
+		return (1);
 	}
 	while (temp && count < index)
 	{
