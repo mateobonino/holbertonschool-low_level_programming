31510ebd054c0538112be55ee53c72f8480d490b
Mateo <mbonino1810@gmail.com>
2022-08-21 17:44:20 -0300

    Task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     ptr = malloc(sizeof(hash_table_t));
     if (!ptr)
         return (NULL);
-    ptr->array = malloc(sizeof(hash_table_t) * size);
+    ptr->array = malloc(sizeof(hash_node_t) * size);
     if (!ptr->array)
     {
         free(ptr);
