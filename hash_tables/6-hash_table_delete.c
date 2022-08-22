164ad6a76e7f2e750fb4551da5ed0b45bb47015a
Mateo <mbonino1810@gmail.com>
2022-08-21 17:30:36 -0700

    task 7

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.c"
+/**
+* hash_table_delete - deletes a hash table
+* @ht: the hash table
+* Return: void func
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int k = 0;
+	hash_node_t *node = NULL, *temp = NULL;
+
+	for (k = 0; k < ht->size; k++)
+	{
+		if (ht->array[k])
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				temp = node;
+				node = node->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+				node = temp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
