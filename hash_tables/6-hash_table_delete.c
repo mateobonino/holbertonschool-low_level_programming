014778c9853e54b8e174842a2d0b75865fc37f8e
Mateo <mbonino1810@gmail.com>
2022-08-21 17:34:35 -0700

    task 7

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,20 +9,18 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int k = 0;
 	hash_node_t *node = NULL, *temp = NULL;
 
+	if (!ht)
+		return;
 	for (k = 0; k < ht->size; k++)
 	{
-		if (ht->array[k])
+		node = ht->array[k];
+		while (node)
 		{
-			node = ht->array[k];
-			while (node)
-			{
-				temp = node;
-				node = node->next;
-				free(temp->key);
-				free(temp->value);
-				free(temp);
-				node = temp;
-			}
+			temp = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			node = temp;
 		}
 	}
 	free(ht->array);
